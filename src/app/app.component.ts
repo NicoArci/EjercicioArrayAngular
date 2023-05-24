0d2e79f5f00ebe7674ac3df04ec5bbd9d4a3cd07
Nicolas Arciniegas <nicolasarci96@gmail.com>
2023-05-24 17:58:17 -0500

    se crea el primero commit del ejercicio de angular solicitado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'listaarticulos';
+
+  productos = [
+    {
+      producto: "Iphone 13",
+      color: "gris",
+      precio: 3000000,
+      camaras: 2,
+      pantalla: "2k"
+    },
+    {
+      producto: "Samsung s22",
+      color: "verde",
+      precio: 2500000,
+      camaras: 3,
+      pantalla: "Oled FullHd"
+    },
+    {
+      producto: "Poco F3",
+      color: "blanco perla",
+      precio: 1500000,
+      camaras: 3,
+      pantalla: "FullHd"
+    }
+  ]
 }
---
+++ src/app/listadoarticulos/listadoarticulos.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-listadoarticulos',
+  templateUrl: './listadoarticulos.component.html',
+  styleUrls: ['./listadoarticulos.component.css']
+})
+export class ListadoarticulosComponent {
+  @Input()productos:any[] = [];
+  constructor(){}
+
+ 
+}
